56890cd4e8294a71919138360b319dc601445b8b
Vyacheslav_Efimov <mrtrondys@gmail.com>
2023-07-25 17:58:25 +0600

    Firs commit

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    public static void build() {
+
+        System.out.println("You must build a new car");
+    }
+
+    public void buy()  {
+
+        System.out.println("I will buy this car");
+    }
+
+    int carspeed =0;
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car.build();
+        Car x = new Car();
+        x.buy();
+    }
+}
